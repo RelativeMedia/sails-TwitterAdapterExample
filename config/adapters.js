afdcf97cd2192f48769c36f12e49c732367392e9
Mike DeVita <mike@gorelative.com>
2013-07-10 15:53:52 -0700

    enabled config/adapter.js

---
+++ config/adapters.js
@@ -0,0 +1,28 @@
+// Configure installed adapters
+// If you define an attribute in your model definition,
+// it will override anything from this global config.
+module.exports.adapters = {
+
+	// If you leave the adapter config unspecified
+	// in a model definition, 'default' will be used.
+	'default': 'disk',
+
+	// In-memory adapter for DEVELOPMENT ONLY
+	memory: {
+		module: 'sails-memory'
+	},
+
+	// Persistent adapter for DEVELOPMENT ONLY
+	// (data IS preserved when the server shuts down)
+	disk: {
+		module: 'sails-disk'
+	},
+
+	//get these keys from your developer profile for twitter
+	twitter: {
+		consumer_key: 'foo',
+		consumer_secret: 'bar',
+		access_token: 'baz',
+		access_token_secret: 'boom'
+	}
+};
