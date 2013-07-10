ae65ff5f54534d760cc103946fcff9295a738d4e
Mike DeVita <mike@gorelative.com>
2013-07-10 15:51:42 -0700

    updated controller example to be more dynamic
    and accept both query params and uri params.

--- api/controllers/TwitterController.js
+++ api/controllers/TwitterController.js
@@ -7,14 +7,40 @@ var TwitterController = {
 
     //Use the Waterline syntax
     var criteria = {
+      where: req.query || req.params
+    };
+
+    if(!criteria.where.screen_name) res.send(500, 'screen_name must be specified');
+    Twitter.adapter.find('timeline', criteria, function(err, result){
+      if(err){
+        console.log.error(err);
+        res.send(err.statusCode);
+      }else{
+        sails.log.debug('search/user_timeline result, ', result);
+        res.send(result);
+      }
+    });
+  },
+
+  search: function(req, res){
+    var criteria = {
       where: {
-        screen_name: req.params.screen_name,
-        count: req.params.count
+        q: req.query.q || req.params.query,
+        count: req.query.count || req.params.count
       }
     };
 
-    Twitter.adapter.find('timeline', criteria, function(err, result){
-      res.send(result);
+    sails.log.debug(criteria);
+
+    Twitter.adapter.find('tweet', criteria, function(err, result){
+      if(err){
+        sails.log.error(err);
+        res.send(err.statusCode);
+      }else{
+        sails.log.debug('search/tweets result, ', result);
+        res.send(result);
+      }
+
     });
   }
 };
